88703b54e5365f7b1b6bd96751b00b590170e9d3
simalaia <example@example.com>
2025-02-26 11:51:15 +0000

    Add files for template repository

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+
+module.exports = {
+    mode: "development",
+    devtool: "eval-source-map",
+    devServer: {
+	watchFiles: ["./src/template.html"],
+    },
+})
